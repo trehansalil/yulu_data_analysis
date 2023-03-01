914e60c9169976ffd8bf946ab35b1c043ef0a700
Salil Trehan <trehansalil1@gmail.com>
2023-03-01 18:25:02 +0530

    Work update

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Yulu Data Analysis
-git config --global user.name "Salil Trehan"
-git config --global user.email "trehansalil1@gmail.com"
--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
 pip install -r requirements.txt
+git config --global user.name "Salil Trehan"
+git config --global user.email "trehansalil1@gmail.com"
